f1c8aaed8b510ba58eababaae939af1e12b72974
Sam Hart <s.w.hart22@gmail.com>
2016-11-07 15:36:21 -0600

    rid of wonky loops, much simpler jquery

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@
 		
 		
   		 function showInfo(data, tabletop) {
-			
-   			console.log(data);
    			 
    			function isNamed(data){
    				return data.name != '';
@@ -21,126 +19,51 @@
    			
    			var nameFilter = data.filter(isNamed);
    			
+   			console.log(data);
+   			
    			console.log( nameFilter );
 
-   			function isBetty (data){
+		for (i=0; i<nameFilter.length; i++){
+			function isTrack (data){
 //update track         			
-        			return data.track == 'betty-johnson';
+        			return data.track ;
         		}
-        		
+/*	        		
         	function isLavi (data){
         		return data.track == 'lavi-conner';
-        	}
+        	}*/
 //update function        		
-        	var bettyFilter = data.filter(isBetty);
+        	var trackFilter = data.filter(isTrack);
         	
-        	var laviFilter = data.filter(isLavi);
+        	/*var laviFilter = data.filter(isLavi);*/
+ 	}
         	
-        	document.getElementById('story01').innerHTML += '<div class="slide"><div class="inner-wrapper" id="landing"></div></div>';
-        	
-        	for (i=0; i < nameFilter.length; i++){
+//create slides
+			for (i=0; i<data.length; i++){
+			
+				if (data[i].type == "landing") {
+				document.getElementById('story01').innerHTML += '<div class="slide"><div class="inner-wrapper landing"></div></div>';
+        	}
+        		else{
+			document.getElementById('story01').innerHTML += '<div class="slide" id="'+data[i].track+'-'+data[i].order+'"><div class="inner-wrapper"><div class="buttons"><button class="prev"><i class="left-arrow" title="arrow icon"></i></button><a class="next" href="#'+data[i].track+'-'+data[i + 1].order+'"><i class="right-arrow" title="arrow icon"></i></a></div><div><p class="quote">' + data[i].text + "</p></div></div></div>"
+			;};
+			}
+			for (i=0; i<data.length; i++){
+				document.getElementsByClassName('slide')[i].style.backgroundImage = 'url('+data[i].img+')';
+			}
+			//create landing 
+   				for (i=0; i < nameFilter.length; i++){
    				
-   				$('#landing').append('<div class="landing-nav" id="'+nameFilter[i].track+'">'+ nameFilter[i].name+'</div>');
+   				$('.landing').append('<a href="#'+nameFilter[i].track+'-1"><div class=" landing-nav">'+ nameFilter[i].name+'</div></a>');
    					
    			}
-        	
-        	function isTrack (data) {
-   					return data.track;
-   				}
+   			
+   			
    				
-   			var trackFilter = data.filter(isTrack);
+   		
+   			}
+   			
         	
-        	$('#'+trackFilter[i].track).click(function(){
         	
-   			for (i = 0; i < nameFilter.length; i++) {
    			
-   			document.getElementById('story01').innerHTML += '<div class="slide"><div class="inner-wrapper"><div class="buttons"><button class="prev"><i class="left-arrow" title="arrow icon"></i></button><button class="next"><i class="right-arrow" title="arrow icon"></i></button></div><div><p class="quote">' + trackFilter[i].text + "</p></div></div></div>";
-   			
-   			var slide = document.getElementsByClassName('slide')[i].style.backgroundImage = "url('"+trackFilter[i].img+"')";
-
-   			$(document).ready(function() {
-					$("#story01 .slide").each(function(e) {
-        				if (e != 0)
-            		$(this).css('display','none');
-    			});
-    			
-    		});
-    			
-    			$(".next").click(function(){
-        			if ($("#story01 div.slide:visible").next().length != 0)
-            			$("#story01 div.slide:visible").next().css('display', 'block').prev().css('display','none');
-        			else {
-            			$("#story01 div.slide:visible").css('display','none');
-            			$("#story01 div.slide:first").css('display','block');
-        		}
-        		return false;
-    			});
-    
-    			$(".prev").click(function(){
-        			if ($("#story01 div.slide:visible").prev().length != 0)
-           			 	$("#story01 div.slide:visible").prev().css('display','block').next().css('display','none');
-        			else {
-            			$("#story01 div.slide:visible").css('display','none');
-            			$("#story01 div.slide:last").css('display','block');
-        		}
-        		return false;
-    			});
-  		}
-  			if ($("#story01 div.slide:visible").next().length != 0)
-            			$("#story01 div.slide:visible").next().css('display', 'block').prev().css('display','none');
-        			else {
-            			$("#story01 div.slide:visible").css('display','none');
-            			$("#story01 div.slide:first").css('display','block');
-        		}
-        		return false;
-  		});
-  			$('.lavi-conner').click(function(){
-        		for (i = 0; i < laviFilter.length; i++) {
-   			
-   			document.getElementById('story01').innerHTML += '<div class="slide"><div class="inner-wrapper"><div class="buttons"><button class="prev"><i class="left-arrow" title="arrow icon"></i></button><button class="next"><i class="right-arrow" title="arrow icon"></i></button></div><div><p class="quote">' + laviFilter[i].text + "</p></div></div></div>";
-   			
-   			var slide = document.getElementsByClassName('slide')[i].style.backgroundImage = "url('"+laviFilter[i].img+"')";
-
-   			$(document).ready(function() {
-					$("#story01 .slide").each(function(e) {
-        				if (e != 0)
-            		$(this).css('display','none');
-    			});
-    			
-    		});
-    			
-    			$(".next").click(function(){
-        			if ($("#story01 div.slide:visible").next().length != 0)
-            			$("#story01 div.slide:visible").next().css('display', 'block').prev().css('display','none');
-        			else {
-            			$("#story01 div.slide:visible").css('display','none');
-            			$("#story01 div.slide:first").css('display','block');
-        		}
-        		return false;
-    			});
-    
-    			$(".prev").click(function(){
-        			if ($("#story01 div.slide:visible").prev().length != 0)
-           			 	$("#story01 div.slide:visible").prev().css('display','block').next().css('display','none');
-        			else {
-            			$("#story01 div.slide:visible").css('display','none');
-            			$("#story01 div.slide:last").css('display','block');
-        		}
-        		return false;
-    			});
-  		};
-  				
-        			if ($("#story01 div.slide:visible").next().length != 0)
-            			$("#story01 div.slide:visible").next().css('display', 'block').prev().css('display','none');
-        			else {
-            			$("#story01 div.slide:visible").css('display','none');
-            			$("#story01 div.slide:first").css('display','block');
-        		}
-        		return false;
-    			
-  				
-  		
-  		});
-		
-	
-	}
+        	
